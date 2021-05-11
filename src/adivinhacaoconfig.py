23d6cd8d7aea362a604101048656017f09a226f9
Guilherme Lyra <lyracampos@gmail.com>
2021-05-11 20:39:22 -0300

    criando classe adivinhacaoconfig para definir configurações do jogo de adivinhação.

---
+++ src/adivinhacaoconfig.py
@@ -0,0 +1,32 @@
+import random
+
+class AdivinhacaoConfigs:
+    
+    def __init__(self, dificuldade):
+        self.__dificuldade = dificuldade
+
+    @property
+    def dificuldade(self):
+        return self.__dificuldade
+
+    @property
+    def numero_secreto(self):
+        return random.randrange(1, 101)
+
+    @property
+    def numero_tentativas(self):
+        return self.calcular_numero_tentativas()
+
+    @property
+    def pontos(self):
+        return 100
+        
+    def calcular_numero_tentativas(self):
+        if (self.__dificuldade == 1):
+            return 20
+        elif (self.__dificuldade == 2):
+            return 10
+        elif (self.__dificuldade == 3):
+            return 5
+        else:
+            return 0
---
+++ src/adivinhacaomenu.py
@@ -0,0 +1,11 @@
+class AdivinhacaoMenu:
+    pass
+
+    def __init__(self):
+        pass
+
+    def menu_inicial(self):
+        print("*********************************")
+        print("Bem vindo ao jogo de adivinhação.")
+        print("*********************************")
+        print("")
